3365937baabe7579be9069d2cf519f9c05c25791
Bob Jacobsen <jacobsen@mac.com>
2017-05-20 21:33:54 -0700

    minor cleanup; unix line eds

--- userfiles/SetCtcIndicators.py
+++ userfiles/SetCtcIndicators.py
@@ -10,12 +10,17 @@ class setStartup(jmri.jmrit.automat.AbstractAutomaton) :
   def init(self):
     return
   def handle(self):
+    # delay long enough for debug init to run if present and polling to start if not
+    self.waitMsec(8000)         # time is in milliseconds
+    
+    print "SetCtcIndicators starts"
+
     # init internal sensors, not present on layout
     sensors.getSensor("TC 20").setState(INACTIVE)
     sensors.getSensor("TC 21").setState(INACTIVE)
     sensors.getSensor("TC 22").setState(INACTIVE)
     sensors.getSensor("TC 23").setState(INACTIVE)
-    #
+
     # init internal sensors for Bell Controls
     sensors.provideSensor("IS-BELL-BLACKBUTTEW").setState(ACTIVE)
     sensors.provideSensor("IS-BELL-BLACKBUTTEE").setState(ACTIVE)
@@ -24,14 +29,14 @@ class setStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.provideSensor("IS-BELL-DORRISW").setState(ACTIVE)
     sensors.provideSensor("IS-BELL-DORRISE").setState(ACTIVE)
     sensors.provideSensor("IS-BELL-TEXUM").setState(ACTIVE)
-    #
+
     # set the default call-on state to off
     turnouts.getTurnout("Call On Mode 38").setState(CLOSED)
     turnouts.getTurnout("Call On Mode 40").setState(CLOSED)
-    #
+
     # set the default state of turnout locks to unlocked
     turnouts.getTurnout("Lock 86").setState(THROWN)
-    #
+
     # set the default state of train order boards to off
     turnouts.getTurnout("TO Signal GL Westbound").setState(CLOSED)
     turnouts.getTurnout("TO Signal GL Eastbound").setState(CLOSED)
@@ -39,11 +44,14 @@ class setStartup(jmri.jmrit.automat.AbstractAutomaton) :
     turnouts.getTurnout("TO Signal BB Westbound").setState(CLOSED)
     turnouts.getTurnout("TO Signal DR Westbound").setState(CLOSED)
     turnouts.getTurnout("TO Signal DR Eastbound").setState(CLOSED)
-    #
-    # Wait for polling, etc, before setting mode
-    self.waitMsec(5000)         # time is in milliseconds
-    # 
-    # push buttons at Grass Lake for main
+
+    # Black Butte lunars on but dark
+    signals.getSignalHead("40 R BB Lunar Left").setAppearance(LUNAR)
+    signals.getSignalHead("40 R BB Lunar Left").setLit(False)
+    signals.getSignalHead("40 R BB Lunar Right").setAppearance(LUNAR)
+    signals.getSignalHead("40 R BB Lunar Right").setLit(False)
+
+    # push & unpush buttons at Grass Lake for main
     sensors.provideSensor("GLE PB Main Track").setState(ACTIVE)
     sensors.provideSensor("GLW PB Main Track").setState(ACTIVE)
     sensors.provideSensor("GLY PB Main").setState(ACTIVE)
@@ -51,13 +59,14 @@ class setStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.provideSensor("GLE PB Main Track").setState(INACTIVE)
     sensors.provideSensor("GLW PB Main Track").setState(INACTIVE)
     sensors.provideSensor("GLY PB Main").setState(INACTIVE)
+    
     # set up the outside signal mast
     turnouts.getTurnout("CT9001").setState(THROWN)
     turnouts.getTurnout("CT9002").setState(THROWN)
     turnouts.getTurnout("CT9003").setState(THROWN)
     turnouts.getTurnout("CT9004").setState(THROWN)
     turnouts.getTurnout("ITSIGNALMASTAUTO").setState(CLOSED)
-    #
+
     # force mode transition to get a stable set of states
     if sensors.getSensor("CTC Mode").state == ACTIVE :
         sensors.getSensor("CTC Mode").setState(INACTIVE)
