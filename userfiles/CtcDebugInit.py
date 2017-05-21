fda95d28c41183b4c38ea8ce453ca6e31310617e
Bob Jacobsen <jacobsen@mac.com>
2017-05-21 08:45:09 -0700

    remove center position from Turnout levers

--- userfiles/CtcDebugInit.py
+++ userfiles/CtcDebugInit.py
@@ -39,7 +39,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     # set the CTC inputs as if machine is present
     # and levers are set to default positions
     sensors.getSensor("CTC 01 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 01 C").setState(INACTIVE)
     sensors.getSensor("CTC 01 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 02 L").setState(INACTIVE)
@@ -47,7 +46,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 02 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 03 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 03 C").setState(INACTIVE)
     sensors.getSensor("CTC 03 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 04 L").setState(INACTIVE)
@@ -55,7 +53,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 04 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 05 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 05 C").setState(INACTIVE)
     sensors.getSensor("CTC 05 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 06 L").setState(INACTIVE)
@@ -63,7 +60,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 06 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 07 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 07 C").setState(INACTIVE)
     sensors.getSensor("CTC 07 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 08 L").setState(INACTIVE)
@@ -71,7 +67,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 08 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 09 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 09 C").setState(INACTIVE)
     sensors.getSensor("CTC 09 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 10 L").setState(INACTIVE)
@@ -79,7 +74,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 10 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 13 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 13 C").setState(INACTIVE)
     sensors.getSensor("CTC 13 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 14 L").setState(INACTIVE)
@@ -91,7 +85,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 16 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 17 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 17 C").setState(INACTIVE)
     sensors.getSensor("CTC 17 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 18 L").setState(INACTIVE)
@@ -103,7 +96,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 20 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 21 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 21 C").setState(INACTIVE)
     sensors.getSensor("CTC 21 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 22 L").setState(INACTIVE)
@@ -111,7 +103,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 22 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 25 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 25 C").setState(INACTIVE)
     sensors.getSensor("CTC 25 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 26 L").setState(INACTIVE)
@@ -119,7 +110,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 26 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 27 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 27 C").setState(INACTIVE)
     sensors.getSensor("CTC 27 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 28 L").setState(INACTIVE)
@@ -127,7 +117,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 28 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 29 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 29 C").setState(INACTIVE)
     sensors.getSensor("CTC 29 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 30 L").setState(INACTIVE)
@@ -135,7 +124,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 30 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 31 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 31 C").setState(INACTIVE)
     sensors.getSensor("CTC 31 R").setState(INACTIVE)
     
     sensors.getSensor("CTC 32 L").setState(INACTIVE)
@@ -147,7 +135,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 34 R").setState(INACTIVE)
 
     sensors.getSensor("CTC 35 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 35 C").setState(INACTIVE)
     sensors.getSensor("CTC 35 R").setState(INACTIVE)
 
     sensors.getSensor("CTC 36 L").setState(INACTIVE)
@@ -159,7 +146,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 38 R").setState(INACTIVE)
 
     sensors.getSensor("CTC 39 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 39 C").setState(INACTIVE)
     sensors.getSensor("CTC 39 R").setState(INACTIVE)
 
     sensors.getSensor("CTC 40 L").setState(INACTIVE)
@@ -167,7 +153,6 @@ class setDebugStartup(jmri.jmrit.automat.AbstractAutomaton) :
     sensors.getSensor("CTC 40 R").setState(INACTIVE)
 
     sensors.getSensor("CTC 41 N").setState(ACTIVE)
-    sensors.getSensor("IS CTC 41 C").setState(INACTIVE)
     sensors.getSensor("CTC 41 R").setState(INACTIVE)
 
     sensors.getSensor("CTC 42 L").setState(INACTIVE)
