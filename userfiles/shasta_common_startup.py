b5ab8b8a75694035ab83500f0d2573ade72bd22c
Bob Jacobsen <jacobsen@mac.com>
2017-05-21 22:27:42 -0700

    panel order at startup

--- userfiles/shasta_common_startup.py
+++ userfiles/shasta_common_startup.py
@@ -11,18 +11,22 @@ cm = jmri.InstanceManager.getDefault(jmri.ConfigureManager)
 execfile(jmri.util.FileUtil.getExternalFilename("program:jython/TurnOffXmlValidation.py"))
 
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:FullConfig.xml")))
-cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:CtcModeControl.xml")))
-cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:CmriNodeStatus.xml")))
+
+cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:TurntableControls.xml")))
+execfile(jmri.util.FileUtil.getExternalFilename("preference:Turntable.py"))
+
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:SignalMastControls.xml")))
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:MtShastaCrossingPanel.xml")))
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:GLWpanel.xml")))
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:GLYpanel.xml")))
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:GLEpanel.xml")))
-cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:BWpanel.xml")))
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:LeftPanel.xml")))
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:MidPanel.xml")))
 cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:OperatorPanel.xml")))
-cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:TurntableControls.xml")))
+
+cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:BWpanel.xml")))
+cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:CtcModeControl.xml")))
+cm.load(java.io.File(jmri.util.FileUtil.getExternalFilename("preference:CmriNodeStatus.xml")))
 
 # start signal followers
 execfile(jmri.util.FileUtil.getExternalFilename("program:jython/SignalFollower.py"))
@@ -30,8 +34,6 @@ SignalFollowerListener().set("CH2001","CH2001R")
 SignalFollowerListener().set("CH2002","CH2002R")
 
 execfile(jmri.util.FileUtil.getExternalFilename("program:jython/CmriNodeMonitor.py"))
-
-execfile(jmri.util.FileUtil.getExternalFilename("preference:Turntable.py"))
 
 execfile(jmri.util.FileUtil.getExternalFilename("preference:SetCtcIndicators.py"))
 
