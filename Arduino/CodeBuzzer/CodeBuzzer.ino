87ddd7912728a37ebb68d53de202a4e78e4e14fe
Bob Jacobsen <jacobsen@mac.com>
2017-07-08 07:41:11 -0700

    clean up, add anti-noise check at start

--- Arduino/CodeBuzzer/CodeBuzzer.ino
+++ Arduino/CodeBuzzer/CodeBuzzer.ino
@@ -2,6 +2,7 @@
 
 int startpin = 10;  // 0 but / 0v makes sound
 int buzzerpin = 13; // 1 bit / 5V makes sound
+int slice = 75; // basic time slice, length of a dot
 
 // Shasta shield uses a 4N25 isolator with input to pin and gnd,
 // load to + and to output emitter
@@ -18,56 +19,57 @@ void setup() {
     digitalWrite(buzzerpin, 0); // ensure off
 }  
 
-int slice = 75;
-
 void loop() {
+    // wait for start
     if (digitalRead(startpin) == 0) {
+      // check for real input
+      delay(50);
+      if (digitalRead(startpin) != 0) return;
+        
+        letterR();
+        interCharDelay();
+        letterD();    
 
-      // write D  (-..)
-      digitalWrite(buzzerpin, 1);
-      delay(slice);
-      delay(slice);
-      digitalWrite(buzzerpin, 0);
-      delay(slice);
-      //
-      digitalWrite(buzzerpin, 1);
-      delay(slice);
-      digitalWrite(buzzerpin, 0);
-      delay(slice);
-      //
-      digitalWrite(buzzerpin, 1);
-      delay(slice);
-      digitalWrite(buzzerpin, 0);
-      delay(slice);
-
-      // inter-char
-      delay(slice);
-      delay(slice);
-      delay(slice);
-    
-      // write R (._..)
-      digitalWrite(buzzerpin, 1);
-      delay(slice);
-      digitalWrite(buzzerpin, 0);
-      delay(slice);
-      // 
-      digitalWrite(buzzerpin, 1);
-      delay(slice);
-      delay(slice);
-      digitalWrite(buzzerpin, 0);
-      delay(slice);
-      //
-      digitalWrite(buzzerpin, 1);
-      delay(slice);
-      digitalWrite(buzzerpin, 0);
-      delay(slice);
-      //
-      digitalWrite(buzzerpin, 1);
-      delay(slice);
-      digitalWrite(buzzerpin, 0);
-      delay(slice);
-
-      // and cycle wait
-      delay(5000); // was 5000
+      // and and wait for rest of cycle length
+      delay(5000);
     }
 }
+
+void dot() {
+    // short (.) plus inter-symbol delay
+    digitalWrite(buzzerpin, 1);
+    delay(slice);
+    digitalWrite(buzzerpin, 0);
+    delay(slice);
+}
+
+void dash() {
+    // long (-) plus inter-symbol delay
+    digitalWrite(buzzerpin, 1);
+    delay(slice);
+    delay(slice);
+    digitalWrite(buzzerpin, 0);
+    delay(slice);
+}
+
+void interCharDelay() {
+    delay(slice);
+    delay(slice);
+    delay(slice);
+}
+
+void letterR() {
+    // Letter R (._..)
+    dot();
+    dash();
+    dot();
+    dot();
+}
+
+void letterD() {
+    // Letter D  (-..)
+    dash();
+    dot();
+    dot();
+}
+
